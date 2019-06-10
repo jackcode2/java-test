d1587d5cbb57b7967657c02e1f51f00a631ebad7
jackcode2 <806510870@qq.com>
2019-06-10 20:36:18 +0800

    大写字母随机生成

---
+++ DAXIE.java
@@ -0,0 +1,10 @@
+package huiwen;
+public class DAXIE {
+	public static void main(String[] args) {
+		int i=(int)Math.round(Math.random()*26);
+		int j=(int)'A'+i;
+		char ch=(char)j;
+				System.out.println(""+ch);
+	}
+
+}
